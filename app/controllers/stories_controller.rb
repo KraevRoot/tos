32ac39384deb98a387258ac17d6117592d468f2b
KraevRoot <Rosenroot@tut.by>
2015-09-20 22:29:46 +0300

    Stories contoller & views added.

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,29 @@
+class StoriesController < ApplicationController
+  def index
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+
+    if @story.save
+      flash[:notice] = "Story successfully created."
+      redirect_to @story
+    else
+      flash[:alert] = "Something going wrong."
+    end
+  end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:title, :description)
+  end
+end
