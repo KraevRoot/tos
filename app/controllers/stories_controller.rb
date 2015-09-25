c8051ef60628ca10ccec989523802a0c624d51f7
KraevRoot <Rosenroot@tut.by>
2015-09-25 23:55:55 +0300

    add update&edit methods for stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -18,6 +18,19 @@ class StoriesController < ApplicationController
     end
   end
 
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  def update
+    @story = Story.find(params[:id])
+    if @story.update(story_params)
+      redirect_to stories_path
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @story = Story.find(params[:id])
   end
