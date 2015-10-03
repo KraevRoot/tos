93e7100b1c535bf74cfd0b7f0118c2a587ea3966
KraevRoot <Rosenroot@tut.by>
2015-10-03 22:45:00 +0300

    delete comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920185951) do
+ActiveRecord::Schema.define(version: 20151003145624) do
 
   create_table "stories", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
