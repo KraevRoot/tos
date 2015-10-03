8931354921bff4a5d26c32a55ef6ec0f988644af
KraevRoot <Rosenroot@tut.by>
2015-10-03 23:04:26 +0300

    beautify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root 'stories#index'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  resources :users
+  resources :sessions
   resources :stories
+
+  root 'stories#index'
 end
