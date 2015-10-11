d20bca31ef56072b8d180605c032cfc5f4cdd334
KraevRoot <Rosenroot@tut.by>
2015-10-11 23:57:30 +0300

    add migration for users table ( add name )

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :password, length: { in: 6..20 }
-  validates :email, :password, :password_confirmation, presence: true
+  validates :name, :email, :password, :password_confirmation, presence: true
   validates_uniqueness_of :email
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
